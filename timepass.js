436b4a299337496c5d6532b60e51a0b497afe0dc
Deep Basnet <deepbasnet68@gmail.com>
2026-06-27 19:33:44 +0545

    Add timepass.js with basic JavaScript concepts and examples

---
+++ timepass.js
@@ -0,0 +1,29 @@
+
+
+let name = "Deep";
+let age = 22;
+let city = "Kathmandu";
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+console.log("I live in " + city);
+
+
+let a = 10;
+let b = 5;
+
+console.log("Sum: " + (a + b));
+console.log("Subtract: " + (a - b));
+console.log("Multiply: " + (a * b));
+console.log("Divide: " + (a / b));
+
+
+let fruits = ["Apple", "Banana", "Mango"];
+console.log("My favourite fruit is " + fruits[2]);
+
+
+if (age >= 18) {
+  console.log("I am an adult");
+} else {
+  console.log("I am a kid");
+}
